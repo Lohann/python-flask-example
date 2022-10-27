1bc22705c9a9732cf47fd36978154720cdd7b68f
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2022-10-27 23:24:22 +0000

    alteracao

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "ola mundo"
+    return "DEU CERTO mundo"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
+
+# sudo apt install python3-pip
+# pip3 install -r requirements.txt
